83a8baf98d1ea27482c91b6ab278413c2ba64816
Josh King <jking@chambana.net>
2020-08-20 16:09:06 -0400

    Update production user seed.

--- src/backend/db/seeds/production/users.js
+++ src/backend/db/seeds/production/users.js
@@ -3,34 +3,33 @@ import bcrypt from 'bcryptjs';
 export function seed(knex) {
   const salt = bcrypt.genSaltSync();
   const hash = bcrypt.hashSync('averylongandgoodpassword', salt);
-  return Promise.all([
-    knex('users')
-      .del()
-      .then(function() {
-        // Inserts seed entries
-        return knex('users').insert([
-            {
-              id: 1,
-              username: 'admin',
-              password: hash,
-              firstName: 'Admin',
-              lastName: 'User',
-              email: 'admin@mlbn.org',
-              phone: '1-555-867-5309',
-              extension: '111',
-            },
-          ]);
-      }),
-    knex('user_groups')
-      .del()
-      .then(function() {
-        // Inserts seed entries
-        return knex('user_groups').insert([
-          {
-            gid: 1,
-            uid: 1,
-          },
-        ]);
-      }),
-  ]);
+  return knex('users')
+    .del()
+    .then(() => {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          id: 1,
+          username: 'admin',
+          password: hash,
+          firstName: 'Admin',
+          lastName: 'User',
+          email: 'admin@mlbn.org',
+          phone: '1-555-867-5309',
+          extension: '111',
+        },
+      ]);
+    })
+    .then(() => {
+      return knex('user_groups').del();
+    })
+    .then(() => {
+      // Inserts seed entries
+      return knex('user_groups').insert([
+        {
+          gid: 1,
+          uid: 1,
+        },
+      ]);
+    });
 }
